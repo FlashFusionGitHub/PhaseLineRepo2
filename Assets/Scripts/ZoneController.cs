1aa5f63d4e8ad55d5a36ed8b6ebfc2089b3392c9
AIE\s171496 <xeno.spam1993@gmail.com>
2018-09-24 16:48:49 +0930

    lots o stuff

--- Assets/Scripts/ZoneController.cs
+++ Assets/Scripts/ZoneController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
+
+using Pixelplacement;
 
 public class ZoneController : MonoBehaviour {
 
@@ -22,7 +25,6 @@ public class ZoneController : MonoBehaviour {
     // Use this for initialization
     void Start () {
         zones = GetComponentsInChildren<CaptureZoneActor>().ToList();
-
         m_percentage = m_startPercentage / 2;
 	}
 	
@@ -36,24 +38,22 @@ public class ZoneController : MonoBehaviour {
             if(progressTimer <= 0)
             {
                 if (zone.owner == CaptureZoneActor.Owner.NONE)
-                    return;
+                    break;
 
                 if (zone.owner == CaptureZoneActor.Owner.TEAM1)
                 {
                     m_percentage -= amount;
-
                     progressBar.fillAmount = m_percentage / m_startPercentage;
                 }
 
                 if (zone.owner == CaptureZoneActor.Owner.TEAM2)
                 {
                     m_percentage += amount;
-
                     progressBar.fillAmount = m_percentage / m_startPercentage;
                 }
 
                 progressTimer = progressTime;
             }
         }
-	}
+    }
 }
