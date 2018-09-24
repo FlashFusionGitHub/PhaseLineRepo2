f30d9633389a6bfe5aae9cc4372bf5bd9ebe9ddc
Rowen <xeno.spam1993@gmail.com>
2018-09-25 00:45:34 +0930

    meh.

--- Assets/FactionSelectionScreenActor.cs
+++ Assets/FactionSelectionScreenActor.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class FactionSelectionScreenActor : MonoBehaviour
 {
-    public Cursor cursor;
     public Image[] images;
+
+    public Cursor cursor;
 
     // Use this for initialization
     void Start()
@@ -16,6 +18,23 @@ public class FactionSelectionScreenActor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    }
 
+    public void OnPointerEnter(int num)
+    {
+        images[num].transform.localScale = new Vector3(1.4f, 1.4f, 0);
+    }
+
+    public void OnPointerExit(int num)
+    {
+        images[num].transform.localScale = new Vector3(1, 1, 0);
+    }
+
+    public void SelectFaction(int num)
+    {
+        Debug.Log("Faction " + num + " Selected");
+
+        images[num].transform.localScale = new Vector3(1, 1, 0);
+        images[num].color = Color.black;
     }
 }
