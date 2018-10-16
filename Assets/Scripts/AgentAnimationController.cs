4fbbae3f54dfc7e98492fb10df000f305043f0ee
SilentMail2 <32690037+SilentMail2@users.noreply.github.com>
2018-10-16 16:57:10 +1030

    Updated things and stuff

---
+++ Assets/Scripts/AgentAnimationController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentAnimationController : MonoBehaviour {
+
+	public Animator[] childAnimators;
+	public bool findAnimatorsButton;
+	public void callTrigger(string trig)
+	{
+		foreach (Animator anim in childAnimators)
+		{
+			anim.SetTrigger (trig);
+		}
+	}
+
+	public void findAnimators ()
+	{
+		childAnimators = GetComponentsInChildren<Animator> ();
+	}
+
+	void OnDrawGizmos()
+	{
+		if (findAnimatorsButton) 
+		{
+			findAnimators ();
+			findAnimatorsButton = false;
+		}
+	}
+}
