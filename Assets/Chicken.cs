27773e9b13d38adb49a54d1fc2462021975cae41
Rowen <xeno.spam1993@gmail.com>
2018-10-24 15:05:03 +1030

    Made some fixes to the chicken AI

--- Assets/Chicken.cs
+++ Assets/Chicken.cs
@@ -3,58 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+/*NPC Chicken brain*/
 public class Chicken : MonoBehaviour {
 
     public float duration;    //the max time of a walking session (set to ten)
     float elapsedTime = 0f; //time since started walk
-    float wait = 0f; //wait this much time
+    public float wait = 0f; //wait this much time
     float waitTime = 0f; //waited this much time
 
     bool move = true; //start moving
 
-    bool angered;
+    bool angered; //the chickens angered state
 
-    GameObject tank;
+    GameObject tank; //the object to chase
 
-    public int chickenChaseSpeed;
-    public float chaseTimeout = 1.0f;
+    public int chickenSpeed; // chickens happy speed (set to five)
+    public int chickenChaseSpeed; // chickens angry speed (set to twenty)
+    public float chaseTimeout = 1.0f; //time to wait before chicken starts chasing (This prevents chicken getting stuck inside of tank)
 
-    public float chaseTime = 5;
+    public float chaseTime; //The time the chicken will chase a tank(set to ten)
     float chaseTimer;
 
+    /*Unity events for triggering animations*/
     public UnityEvent onChaseStart;
     public UnityEvent onChaseEnd;
     public UnityEvent onMoveStart;
     public UnityEvent onMoveEnd;
 
-    Vector3 target;
+    Vector3 target; //The next target the chicken will translate too
 
     void Start()
     {
-        target = transform.position + new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5));
-
+        target = transform.position;
         chaseTimer = chaseTime;
     }
 
     void Update()
     {
+        //Chicken has fallen of the map, destory the chicken
+        if(transform.position.y < -500.0f) {
+            Destroy(gameObject);
+        }
+
         if (!angered)
         {
             if (move)
             {
                 if (elapsedTime < duration)
                 {
-                    //if its moving and didn't move too much
-                    transform.position = Vector3.MoveTowards(transform.position, target, 5 * Time.deltaTime);
-
+                    //move to the target location, look at the target location, increment time
+                    transform.position = Vector3.MoveTowards(transform.position, target, chickenSpeed * Time.deltaTime);
                     transform.LookAt(target);
-
                     elapsedTime += Time.deltaTime;
                 }
                 else
                 {
                     //do not move and start waiting for random time
-                    wait = Random.Range(2, 5);
+                    wait = Random.Range(2, 4);
                     waitTime = 0f;
                     move = false;
                     onMoveEnd.Invoke();
@@ -63,6 +68,7 @@ public class Chicken : MonoBehaviour {
 
             if(!move)
             {
+                //increment wait time
                 waitTime += Time.deltaTime;
            
                 if(waitTime > wait)
@@ -70,7 +76,7 @@ public class Chicken : MonoBehaviour {
                     move = true;
                     onMoveStart.Invoke();
                     elapsedTime = 0f;
-                    target = transform.position + new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5));
+                    target = transform.position + new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
                 }
             }
         }
@@ -97,8 +103,10 @@ public class Chicken : MonoBehaviour {
 
     private void OnTriggerEnter(Collider collider)
     {
+        //did we collide with a troop actor?
         if(collider.gameObject.GetComponent<TroopActor>())
         {
+            //set tank to the collision entity and invoke MakeAngry()
             tank = collider.gameObject.GetComponent<TroopActor>().gameObject;
             Invoke("MakeAngry", chaseTimeout);
         }
