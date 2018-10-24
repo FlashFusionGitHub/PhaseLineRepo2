2e6405698efb06f7bdb2f789ce6646e6a1334a60
AIE\s162318 <mmatthewspatch1@gmail.com>
2018-10-24 12:53:03 +1030

    Culled Some of the explosions that dont work in game

--- Assets/AudioAgent.cs
+++ Assets/AudioAgent.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioAgent : MonoBehaviour {
 
@@ -9,15 +10,17 @@ public class AudioAgent : MonoBehaviour {
     {
         public string audioTag;
         public AudioSource aSource;
+        public AudioMixerGroup amg;
         public AudioClip[] audioClips;
         public float minPitch = 1;
         public float maxPitch = 2;
 
         public void PlayRandomClip()
         {
-            if (aSource && audioClips.Length > 0 && audioTag != "")
+            if (aSource && audioClips.Length > 0 )
             {
                 aSource.pitch = Random.Range(minPitch, maxPitch);
+                aSource.outputAudioMixerGroup = amg ? amg : aSource.outputAudioMixerGroup;
                 aSource.clip = audioClips [Random.Range(0, audioClips.Length)];
                 aSource.Play();
             }
@@ -37,5 +40,7 @@ public class AudioAgent : MonoBehaviour {
                 return;
             }
         }
+
+        Debug.Log("no audio found for audio tag: " + tagToPlay);
     }
 }
