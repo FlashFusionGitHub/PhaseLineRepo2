bc13d843d8d5d700219429a21a4e5fd5fd58cf85
AIE\s162318 <mmatthewspatch1@gmail.com>
2018-11-06 14:46:05 +1030

    Updated Scene to Have Pick Ups, Updated Scene so that cursor is no longer ontop of capture zones

---
+++ Assets/Scripts/Pick Up Scripts/PickUp.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PickUp : MonoBehaviour {
+
+    public UnityEvent OnColEnter;
+    public TroopActor lastCollided;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        TroopActor ta = collision.gameObject.GetComponent<TroopActor>();
+        if (ta)
+        {
+            lastCollided = ta;
+            OnColEnter.Invoke();
+        }
+    }
+
+    public void RandomBuff(float percentageIncrease)
+    {
+        if (lastCollided)
+            lastCollided.AddRandomBuff(percentageIncrease);
+    }
+    public void HealthBuff(float percentageIncrease)
+    {
+        if (lastCollided)
+            lastCollided.AddHealth(percentageIncrease);
+    }
+    public void RangeBuff(float percentageIncrease)
+    {
+        if (lastCollided)
+            lastCollided.AddRange(percentageIncrease);
+    }
+    public void InfluenceBuff(float percentageIncrease)
+    {
+        if (lastCollided)
+            lastCollided.AddInfluence(percentageIncrease);
+    }
+    public void DamageBuff(float percentageIncrease)
+    {
+        if (lastCollided)
+            lastCollided.AddDamage(percentageIncrease);
+    }
+}
