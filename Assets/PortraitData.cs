d089d019a3626059eceba3432cfc3b3ba0302730
AIE\s162318 <mmatthewspatch1@gmail.com>
2018-11-07 10:30:37 +1030

    Fixed Issues with collider on CaptureZones, Made Base UI dynamic along with unit selection

---
+++ Assets/PortraitData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PortraitData : MonoBehaviour {
+
+    public Sprite baseFace;
+    public Color TeamColor;
+    public Team team;
+
+}
