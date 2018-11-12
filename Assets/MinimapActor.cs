c45f175f355a41450ef346e3a26a00217fe6158d
Rowen <xeno.spam1993@gmail.com>
2018-11-12 22:51:13 +1030

    Minimap.

---
+++ Assets/MinimapActor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapActor : MonoBehaviour {
+
+    public NavigationArrowActor navigationArrowActor;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        
+	}
+}
