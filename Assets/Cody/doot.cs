f60b6e7179a27822ae8f4c8e24aa8d63a08b3c9f
CodyCammXD <cody.camm56@gmail.com>
2018-11-13 16:53:43 +1030

    Complete? main menu

---
+++ Assets/Cody/doot.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doot : MonoBehaviour {
+
+
+	public GameObject playEvent1;
+
+
+    public GameObject start;
+    public GameObject play;
+    public GameObject playtwo;
+
+
+	void Start()
+	{
+        play.SetActive(false);
+
+        if (playtwo != null)
+            playtwo.SetActive(false);
+
+	}
+
+	void OnTriggerEnter(Collider col)
+
+	{
+        Debug.Log("i hit this bitch");
+		if (col.gameObject.tag == "next")
+		{
+            play.SetActive(true);
+
+            if (playtwo != null)
+                playtwo.SetActive(true);
+
+		}
+	}
+}
